1f20657aca0f05b6c5953c48a2166bc7d7ed2b1d
Haythem-Mastour <haythemmastour102@gmail.com>
2023-12-13 16:36:46 +0100

    aaa

--- vgg_service/app/app.py
+++ vgg_service/app/app.py
@@ -1,58 +1,46 @@
+# svm_service/app.py
 from flask import Flask, request, jsonify
+import time
 import os
 import librosa
 import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.models import Model
-import cv2
 
 app = Flask(__name__)
-
 # Get the absolute path to the directory of this script
 base_path = os.path.abspath(os.path.dirname(__file__))
 
-# Load the pre-trained VGG16 model without the top (fully connected) layers
-vgg16_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-
-# Flatten the output of VGG16 and add dense layers for classification
-x = vgg16_model.output
-x = Flatten()(x)
-x = Dense(256, activation='relu')(x)
-predictions = Dense(len(genre_dict), activation='softmax')(x)
-
-# Create a new model with VGG16 as the base and the added dense layers
-model = Model(inputs=vgg16_model.input, outputs=predictions)
-
-# Load the weights of your pre-trained SVM model
+# Load the pre-trained model using joblib
 model_path = os.path.join(base_path, "vgg-classification-model.h5")
-model.load_weights(model_path)
+model = load_model(model_path)
 
-genre_dict = {0: 'blues', 1: 'classical', 2: 'country', 3: 'disco', 4: 'hiphop', 5: 'jazz', 6: 'metal', 7: 'pop', 8: 'reggae', 9: 'rock'}
+genre_dict={0: 'blues', 1: 'classical', 2: 'country', 3: 'disco', 4: 'hiphop', 5: 'jazz', 6: 'metal', 7: 'pop', 8: 'reggae', 9: 'rock'}
+
+# Function to retrieve the latest audio file from a directory
+def get_latest_audio_file(directory):
+    files = [os.path.join(directory, f) for f in os.listdir(
+        directory) if f.endswith('.wav')]  # Change extension if different
+    if files:
+        # Get the latest file based on creation time
+        return max(files, key=os.path.getctime)
+    else:
+        return None
 
 def extract_features(file_path):
     # Load audio file with librosa
     audio, sample_rate = librosa.load(file_path, res_type='kaiser_fast')
-    # Convert audio to spectrogram (image)
-    spec = librosa.feature.melspectrogram(y=audio, sr=sample_rate)
-    spec = librosa.power_to_db(spec, ref=np.max)
+    # Extract MFCCs and other features...
+    mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=20)
+    mfccs_processed = np.mean(mfccs.T,axis=0)
 
-    # Resize the spectrogram to match VGG16 input shape (224x224)
-    spec = cv2.resize(spec, (224, 224), interpolation=cv2.INTER_CUBIC)
-    spec = np.stack((spec, spec, spec), axis=-1)  # Convert to 3-channel image
-
-    # Preprocess the input for VGG16
-    spec = preprocess_input(spec)
-
-    return spec
+    return mfccs_processed
 
 def predict_genre(file_path):
     # Extract features
     features = extract_features(file_path)
 
     # Reshape features to fit the model input format
-    features = np.reshape(features, (1, 224, 224, 3))  # Reshape for VGG16
+    features = np.reshape(features, (1, -1)) # Reshape for model
 
     # Predict genre
     prediction = model.predict(features)
@@ -61,6 +49,7 @@ def predict_genre(file_path):
     predicted_genre = np.argmax(prediction)
     predicted_genre_name = genre_dict.get(predicted_genre, "Unknown Genre")
     return predicted_genre_name
+
 
 @app.route('/classify', methods=['POST'])
 def classify():
@@ -72,7 +61,7 @@ def classify():
     uploaded_file.save(file_path)
 
     genre = predict_genre(file_path)
-    result = "Predicted Genre: " + genre
+    result = "Predicted Genre:" + genre
 
     # Respond with the file name
     response_data = {"received_message": "File received successfully", "response": result}
